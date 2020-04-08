3881a7d5f0809bed48f5970938bc42aaea3c7907
isabel k. lee <hello@isabelklee.co>
2020-04-08 10:45:33 -0400

    create signup form

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,4 +1,7 @@
 class Signup < ApplicationRecord
     belongs_to :camper
     belongs_to :activity
+
+    validates :time, numericality: true
+    validates :time, numericality: { less_than: 23 }
 end

b18d69014d032a8331fdf361ddf81408111696e7
isabel k. lee <hello@isabelklee.co>
2020-04-08 10:27:06 -0400

    create show pages for camper and activity

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -2,4 +2,14 @@ class CampersController < ApplicationController
   def index
     @campers = Camper.all
   end
+
+  def show
+    find_camper
+  end
+
+  private
+  def find_camper
+    @camper = Camper.find(params[:id])
+  end
+
 end

51f39ccbb700b7a6bdd4a078c327db8370326477
Jackmt9 <jackmt9@gmail.com>
2020-04-08 12:16:44 -0400

    My solution

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_175158) do
+ActiveRecord::Schema.define(version: 2020_04_08_151129) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2019_01_02_175158) do
   create_table "campers", force: :cascade do |t|
     t.string "name"
     t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "signups", force: :cascade do |t|
+    t.integer "camper_id"
+    t.integer "activity_id"
+    t.integer "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

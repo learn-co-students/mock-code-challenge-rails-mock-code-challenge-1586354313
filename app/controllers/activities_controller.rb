5e7aa352363ab2065407f98068dbd84009f52f4d
replacem3nts <donny.landis@gmail.com>
2020-04-08 12:31:13 -0400

    updates files to meet deliverables

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,4 +2,14 @@ class ActivitiesController < ApplicationController
   def index
     @activities = Activity.all
   end
+
+  def show
+    @activity = find_activity
+  end
+
+  private
+
+  def find_activity
+    Activity.find(params[:id])
+  end
 end

70cdf875bccef573eca44189cd54f378ae353e42
isabel k. lee <hello@isabelklee.co>
2020-04-08 10:50:24 -0400

    add new camper functionality

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -7,9 +7,27 @@ class CampersController < ApplicationController
     find_camper
   end
 
+  def new
+    @camper = Camper.new
+  end
+
+  def create
+    @camper = Camper.new(camper_params)
+
+    if @camper.save
+      redirect_to @camper
+    else
+      render :new
+    end
+  end
+
   private
   def find_camper
     @camper = Camper.find(params[:id])
+  end
+
+  def camper_params
+    params.require(:camper).permit(:name, :age)
   end
 
 end
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -2,6 +2,5 @@ class Signup < ApplicationRecord
     belongs_to :camper
     belongs_to :activity
 
-    validates :time, numericality: true
     validates :time, numericality: { less_than: 23 }
 end
